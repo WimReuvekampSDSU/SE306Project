d7d18ece3f8abd5708065678d94d34be75d3cb5d
wredshaw <wyattred@hotmail.com>
2023-04-16 16:29:55 -0500

    Commit 2

--- bbay/urls.py
+++ bbay/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('seproject.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
