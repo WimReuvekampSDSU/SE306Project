33878b3414f85fb3df6a3deeab5cc3a39b105e58
wredshaw <wyattred@hotmail.com>
2023-04-17 13:10:00 -0500

    Commit 3

--- bbay/settings.py
+++ bbay/settings.py
@@ -130,4 +130,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = 'seproject.User'
+AUTH_USER_MODEL = 'seproject.CustomUser'
