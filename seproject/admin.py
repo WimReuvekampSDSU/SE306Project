0167a79218de48c814beefc361b55b31cf66c33c
wredshaw <wyattred@hotmail.com>
2023-04-18 11:01:04 -0500

    Commit 2

--- seproject/admin.py
+++ seproject/admin.py
@@ -4,8 +4,3 @@ from .models import Item
 from django.contrib.auth.models import User
 # Register your models here.
 admin.site.register(Item)
-
-class MyUserAdmin(UserAdmin):
-    list_display = ('email', 'username', 'is_staff')
-
-admin.site.register(User, MyUserAdmin)
